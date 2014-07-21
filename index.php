7abd5a610664c9a46439ad9611c33d41f7f35d40
unknown <bolanlr1@ndeuvwv05b00824.gold.rtgold.nima.mil>
2014-07-21 15:03:19 -0400

    Fixed login snafu

--- index.php
+++ index.php
@@ -1,56 +1,164 @@
-<?php
-    require("config.php");
-    if(empty($_SESSION['user'])) 
-    {
-        header("Location: index.php");
-        die("Redirecting to index.php"); 
-    }
-?>
+<?php 
+    require("config.php"); 
+    $submitted_username = ''; 
+    if(!empty($_POST)){ 
+        $query = " 
+            SELECT 
+                id, 
+                username, 
+                password, 
+                salt, 
+                email 
+            FROM users 
+            WHERE 
+                username = :username 
+        "; 
+        $query_params = array( 
+            ':username' => $_POST['username'] 
+        ); 
+         
+        try{ 
+            $stmt = $db->prepare($query); 
+            $result = $stmt->execute($query_params); 
+        } 
+        catch(PDOException $ex){ die("Failed to run query: " . $ex->getMessage()); } 
+        $login_ok = false; 
+        $row = $stmt->fetch(); 
+        if($row){ 
+            $check_password = hash('sha256', $_POST['password'] . $row['salt']); 
+            for($round = 0; $round < 65536; $round++){
+                $check_password = hash('sha256', $check_password . $row['salt']);
+            } 
+            if($check_password === $row['password']){
+                $login_ok = true;
+            } 
+        } 
 
+        if($login_ok){ 
+            unset($row['salt']); 
+            unset($row['password']); 
+            $_SESSION['user'] = $row;  
+            header("Location: hexagon.php"); 
+            die("Redirecting to: hexagon.php"); 
+        } 
+        else{ 
+            print("Login Failed."); 
+            $submitted_username = htmlentities($_POST['username'], ENT_QUOTES, 'UTF-8'); 
+        } 
+    } 
+?> 
 <!DOCTYPE html>
-<head>
-    <title>Hex</title>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Hexagons"><title>Hexagons</title>
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
 	<script src='http://code.jquery.com/jquery-1.10.2.min.js' language='Javascript' type='text/javascript'></script>
-    
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
 	<style>
-	.Game {
-		position: relative;
-		border: 1px solid orange;
-		width: 1200px;
-		height: 900px;
-	}
-	.Game > canvas {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-	}
-	.Game > .controls {
-		position: absolute;
-		bottom: 850px;
-		left: 1000px;
-		height: 40px;
-	}
-	.Game > .controls > button {
-		font-size: 18px;
-		box-sizing: border-box;
-		margin: 4px;
-		border: 1px solid #000;
-		background: #FFCC00;
-	}
+	.form-signin
+{
+    max-width: 330px;
+    padding: 15px;
+    margin: 0 auto;
+}
+.form-signin .form-signin-heading, .form-signin .checkbox
+{
+    margin-bottom: 10px;
+}
+.form-signin .checkbox
+{
+    font-weight: normal;
+}
+.form-signin .form-control
+{
+    position: relative;
+    font-size: 16px;
+    height: auto;
+    padding: 10px;
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+}
+.form-signin .form-control:focus
+{
+    z-index: 2;
+}
+.form-signin input[type="text"]
+{
+    margin-bottom: -1px;
+    border-bottom-left-radius: 0;
+    border-bottom-right-radius: 0;
+}
+.form-signin input[type="password"]
+{
+    margin-bottom: 10px;
+    border-top-left-radius: 0;
+    border-top-right-radius: 0;
+}
+.account-wall
+{
+    margin-top: 20px;
+    padding: 40px 0px 20px 0px;
+    background-color: #f7f7f7;
+    -moz-box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+    -webkit-box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+    box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+}
+.login-title
+{
+    color: #555;
+    font-size: 18px;
+    font-weight: 400;
+    display: block;
+}
+.profile-img
+{
+    width: 96px;
+    height: 96px;
+    margin: 0 auto 10px;
+    display: block;
+    -moz-border-radius: 50%;
+    -webkit-border-radius: 50%;
+    border-radius: 50%;
+}
+.need-help
+{
+    margin-top: 10px;
+}
+.new-account
+{
+    display: block;
+    margin-top: 10px;
+}
 	</style>
 </head>
 <body>
-	<div class=Game>
-		<canvas id="HexCanvas" width="1200" height="900"></canvas>
-		<div class="controls" id="controls" style="display:none">
-			<button type="button" id="singleAttack">Single Attack</button>
-			<button type="button" id ="continuousAttack">Continuous Attack</button>
-		</div>
-	</div>
-	<script src="js/hexagon.js"></script>
-	 <h2>Welcome <?php echo htmlentities($_SESSION['user']['username'], ENT_QUOTES, 'UTF-8'); ?>!</h2>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-6 col-md-4 col-md-offset-4">
+            <h1 class="text-center login-title">Sign in to continue</h1>
+            <div class="account-wall">
+                <img class="profile-img" src="css/photo.png"
+                    alt="">
+                <form class="form-signin" method="post" action="index.php">
+                <input type="text" name="username" class="form-control" placeholder="Username" required autofocus>
+                <input type="password" name="password" class="form-control" placeholder="Password" required>
+                <button class="btn btn-lg btn-primary btn-block" type="submit">
+                    Sign in</button>
+                <label class="checkbox pull-left">
+                    <input type="checkbox" value="remember-me">
+                    Remember me
+                </label>
+                <a href="#" class="pull-right need-help">Need help? </a><span class="clearfix"></span>
+                </form>
+            </div>
+            <a href="register.php" class="text-center new-account">Create an account </a>
+        </div>
+    </div>
+</div>
 </body>
-
-
-   
-
+</html>
--- register.php
+++ register.php
@@ -85,35 +85,105 @@
 	<link href="css/bootstrap.min.css" rel="stylesheet">
 	<script src='http://code.jquery.com/jquery-1.10.2.min.js' language='Javascript' type='text/javascript'></script>
 	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+		<style>
+	.form-signin
+{
+    max-width: 330px;
+    padding: 15px;
+    margin: 0 auto;
+}
+.form-signin .form-signin-heading, .form-signin .checkbox
+{
+    margin-bottom: 10px;
+}
+.form-signin .checkbox
+{
+    font-weight: normal;
+}
+.form-signin .form-control
+{
+    position: relative;
+    font-size: 16px;
+    height: auto;
+    padding: 10px;
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+}
+.form-signin .form-control:focus
+{
+    z-index: 2;
+}
+.form-signin input[type="text"]
+{
+    margin-bottom: -1px;
+    border-bottom-left-radius: 0;
+    border-bottom-right-radius: 0;
+}
+.form-signin input[type="password"]
+{
+    margin-bottom: 10px;
+    border-top-left-radius: 0;
+    border-top-right-radius: 0;
+}
+.account-wall
+{
+    margin-top: 20px;
+    padding: 40px 0px 20px 0px;
+    background-color: #f7f7f7;
+    -moz-box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+    -webkit-box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+    box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+}
+.login-title
+{
+    color: #555;
+    font-size: 18px;
+    font-weight: 400;
+    display: block;
+}
+.profile-img
+{
+    width: 96px;
+    height: 96px;
+    margin: 0 auto 10px;
+    display: block;
+    -moz-border-radius: 50%;
+    -webkit-border-radius: 50%;
+    border-radius: 50%;
+}
+.need-help
+{
+    margin-top: 10px;
+}
+.new-account
+{
+    display: block;
+    margin-top: 10px;
+}
+	</style>
 </head>
 <body>
 
-<form class="form-horizontal" method="post" action="register.php">
- <div class="control-group">
-	<label class="control-label" for="username">Email</label>
-	<div class="controls">
-		<input type="text" name="email" id="email" placeholder="Email">
-	</div>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-6 col-md-4 col-md-offset-4">
+            <h1 class="text-center login-title">Create an account</h1>
+            <div class="account-wall">
+                <img class="profile-img" src="css/photo.png"
+                    alt="">
+                <form class="form-signin" method="post" action="register.php">
+                <input type="text" name="email" class="form-control" placeholder="Email" required autofocus>
+				<input type="text" name="username" class="form-control" placeholder="Username" required>
+                <input type="password" name="password" class="form-control" placeholder="Password" required>
+                <button class="btn btn-lg btn-primary btn-block" type="submit">
+                    Submit</button>
+                <a href="#" class="pull-right need-help">Need help? </a><span class="clearfix"></span>
+                </form>
+            </div>
+        </div>
+    </div>
 </div>
-<div class="control-group">
-	<label class="control-label" for="username">Username</label>
-	<div class="controls">
-		<input type="text" name="username" id="username" placeholder="Username">
-	</div>
-</div>
-<div class="control-group">
-	<label class="control-label" for="password">Password</label>
-	<div class="controls">
-		<input type="password" name="password" id="password" placeholder="Password">
-	</div>
-</div>
-<div class="control-group">
-	<div class="controls">
-		<input name="Submit" type="submit" id="submit" value="Submit" class="btn btn-success"/>
-		<input type="reset" name="Reset" value="Reset" class="btn"/>
-	</div>
-</div>
-</form>
 
 </body>
 </html>
