e980178b90ff7f4a52e717139624bc6427450d6b
unknown <bolanlr1@ndeuvwv05b02409.gold.rtgold.nima.mil>
2014-10-13 16:17:14 -0400

    Working on initial placement

--- js/acceptInvite.js
+++ js/acceptInvite.js
@@ -1,3 +1,22 @@
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex ;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
 function acceptInvite () {
     var data = { param: 'getAll', gameID: $('#game_id').val(), color: $('#colorpicker').val()};
 	$.ajax({
@@ -43,26 +62,22 @@ function acceptInvite () {
 }
 
 function startGame(gameID, mapArray, mapProperties) {
-	//Roll dice to determine order of placement
-	var rolls = [];
-	var order = []; //separate array to keep order of rolls
+	var order = []; //track order
 	for(i=0;i<mapProperties.owners.length;i++){
-		var tmp = rollDice();
-		rolls.push(tmp);
-		order.push(tmp);
+		order[i] = mapProperties.owners[i];
 	}
-	rolls = rolls.sort(function(a, b){return b-a});
+	//Randomize order of owners
+	shuffle(order);
 	var owners = [];
 	var colors = [];
-	for(i=0;i<mapProperties.owners.length;i++){ //Sort owners/colors by rolls
+	for(i=0;i<mapProperties.owners.length;i++){ //Update owners/colors based on shuffle
 		for(j=0;j<order.length;j++){
-			if(rolls[i] == order[j]){
-				owners[i] = mapProperties.owners[j];
-				colors[i] = mapProperties.colors[j];
+			if(mapProperties.owners[i] == order[j]){
+				colors[j] = mapProperties.colors[i];
 			}
 		}
 	}
-	mapProperties.owners = owners;
+	mapProperties.owners = order;
 	mapProperties.colors = colors;
 
     //Scan Map, Count number of Land pieces
@@ -74,24 +89,70 @@ function startGame(gameID, mapArray, mapProperties) {
             }
         }
     }
-	console.log(countries);
-	//randomly shuffle countries array
-	//for (var i, tmp, n = countries.length; n; i = Math.floor(Math.random() * n), tmp = countries[--n], countries[n] = countries[i], countries[i] = tmp);
-	//console.log(countries);
+
     //Initial Troop Placement (Country Claiming). Randomly place 1 unit until they're all filled
     var turn = 0;
+	var cycle = 0;
+	var maxCycles = (countries.length*3)/mapProperties.owners.length;
 	console.log("Length:", mapProperties.owners);
-    for(i=0;i<countries.length;i++){
+
+	//Fill all countries with 1 unit
+	for(i=0;i<countries.length;i++){
 		if(countries[i].owner == ""){
 			countries[i].owner = mapProperties.owners[turn];
+			countries[i].units++;
+			if(turn == (mapProperties.owners.length-1)){
+				turn = 0;
+				cycle++;
+			}else{
+				turn++;   
+			}
 		}
-		countries[i].units++;
+	}
+	
+	//Figure out first country in list available for owner who's turn it is
+	function nextTurn (owner){
+		var nxTurn = 0;
+		for(i=0;i<countries.length;i++){
+			if(countries[i].owner == owner){
+				return i;
+			}
+		}
+	}
+	nxTurn = nextTurn(mapProperties.owners[turn]);
+	
+	while(cycle<maxCycles){
+		console.log("Turn:", turn, " " , nxTurn, " Cycle:", cycle);
+		countries[nxTurn].units++;
 		if(turn == (mapProperties.owners.length-1)){
 			turn = 0;
+			cycle++;
 		}else{
 			turn++;   
 		}
-    }
+		if(nxTurn<countries.length-1){
+			nxTurn++;
+		}else{
+			nxTurn = nextTurn(mapProperties.owners[turn]);
+		}
+	}
+	
+	var cnt1 = 0; var cnt2 = 0; var cnt3 = 0;
+	for(i=0;i<countries.length;i++){
+		if(countries[i].owner=="bo_knows@cfiresim.com"){
+			cnt1 = cnt1 + countries[i].units;
+		}
+		if(countries[i].owner=="bo_knows3@cfiresim.com"){
+			cnt2 = cnt2 + countries[i].units;
+		}
+		if(countries[i].owner=="lawrence.boland@gmail.com"){
+			cnt3 = cnt3 + countries[i].units;
+		}
+	}
+	console.log("Counts: " , cnt1, " ", cnt2, " ", cnt3);
+
+	console.log("MaxCycles:", maxCycles);
+	console.log("End cycles:", cycle);
 	//Initial Troop Placement (Fill up countries)
 	
 	console.log(countries);
